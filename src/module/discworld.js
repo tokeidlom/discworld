592e2e6c694365cce031891c3e104e99c3f3d51c
CalinStar <paul@projectmanageit.co.uk>
2024-10-19 11:38:03 +0100

    Localisation of UI messages

--- src/module/discworld.js
+++ src/module/discworld.js
@@ -1,35 +1,27 @@
-import {
-  DiscRoller
-} from './apps/discroller.js';
-import {
-  DiscworldCharacterSheet
-} from './actors/character-sheet.js';
-import {
-  DiscworldNPCSheet
-} from './actors/npc-sheet.js';
-import {
-  DiscworldTraitsItem
-} from './items/traits-sheet.js';
+import { DiscRoller } from './apps/discroller.js';
+import { DiscworldCharacterSheet } from './actors/character-sheet.js';
+import { DiscworldNPCSheet } from './actors/npc-sheet.js';
+import { DiscworldTraitsItem } from './items/traits-sheet.js';
 
-//Register sheets
-  Actors.unregisterSheet('core', ActorSheet);
-  Items.unregisterSheet('core', ItemSheet);
-  Actors.registerSheet("core", DiscworldCharacterSheet, {
-    types: ["character"],
-    makeDefault: true
-  });
-  Actors.registerSheet("core", DiscworldNPCSheet, {
-    types: ["NPC"],
-  });
-  Items.registerSheet("discworld", DiscworldTraitsItem, {
-    types: ["core", "trait", "quirk", "niche"],
-  });
+// Register sheets
+Actors.unregisterSheet('core', ActorSheet);
+Items.unregisterSheet('core', ItemSheet);
+Actors.registerSheet("core", DiscworldCharacterSheet, {
+  types: ["character"],
+  makeDefault: true
+});
+Actors.registerSheet("core", DiscworldNPCSheet, {
+  types: ["NPC"],
+});
+Items.registerSheet("discworld", DiscworldTraitsItem, {
+  types: ["core", "trait", "quirk", "niche"],
+});
 
-//Item type hooks
+// Item type hooks
 Hooks.on("preCreateItem", (item, options, userId) => {
 
   // Update item images if the default Foundry icon is present
-  if ((!item.img || item.img === "icons/svg/item-bag.svg")) {
+  if (!item.img || item.img === "icons/svg/item-bag.svg") {
     switch (item.type) {
       case "niche":
         item.updateSource({ img: "systems/discworld/assets/items/niches.webp" });
@@ -45,6 +37,7 @@ Hooks.on("preCreateItem", (item, options, userId) => {
         break;
     }
   }
+
   // Define the forbidden items for different actor types
   const actor = item.parent;
   if (!actor || actor.documentName !== "Actor") return true;
@@ -54,12 +47,18 @@ Hooks.on("preCreateItem", (item, options, userId) => {
   const forbiddenItemsForNPC = ["niche", "core", "quirk"];
 
   if (actorType === "character" && forbiddenItemsForCharacter.includes(item.type)) {
-    ui.notifications.error(`${actor.name} cannot hold ${item.type} items.`);
+    ui.notifications.error(game.i18n.format("application.actorcannothold", {
+      actor: actor.name,
+      item: item.type
+    }));
     return false;
   }
 
   if (actorType === "NPC" && forbiddenItemsForNPC.includes(item.type)) {
-    ui.notifications.error(`${actor.name} cannot hold ${item.type} items.`);
+    ui.notifications.error(game.i18n.format("application.actorcannothold", {
+      actor: actor.name,
+      item: item.type
+    }));
     return false;
   }
 
