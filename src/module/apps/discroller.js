6b6481ab6e26c377819134509a0bd93894eed098
CalinStar <paul@projectmanageit.co.uk>
2024-10-22 11:52:38 +0100

    more v13

--- src/module/apps/discroller.js
+++ src/module/apps/discroller.js
@@ -1,57 +1,38 @@
 export class DiscRoller {
-  static async Init(controls, html) {
-    // Create the main dice roll button
-    const diceRollbtn = $(`
-      <li class="scene-control discworld-roller" data-control="DiscRoller" title="${game.i18n.localize('application.discworlddiceroller')}">
-        <img src="/systems/discworld/assets/dice/fancy-dice.png" alt="${game.i18n.localize('application.discworlddiceroller')}" class="custom-icon">
-        <ol class="nested-buttons sub-controls control-tools"></ol>
-      </li>
-    `);
-
-    html.find('.main-controls').append(diceRollbtn);
-
-    diceRollbtn.on('click', (ev) => {
-      this.CreateDiceRoller(ev);
-    });
-  }
 
   static async CreateDiceRoller(event) {
     const dialog = new Dialog({
       title: `${game.i18n.localize('application.discworlddiceroller')}`,
       content: `
         <form>
-          <div class="dice-roll-container">
-            <div class="dice-roll-buttons">
-              <button type="button" class="roll-button" data-dice="d4">
-                <img src="/systems/discworld/assets/dice/dice.png" alt="d4" class="dice-icon">
-                ${game.i18n.localize('application.roll')} d4
-              </button>
-              <button type="button" class="roll-button" data-dice="d6">
-                <img src="/systems/discworld/assets/dice/dice.png" alt="d6" class="dice-icon">
-                ${game.i18n.localize('application.roll')} d6
-              </button>
-              <button type="button" class="roll-button" data-dice="d8">
-                <img src="/systems/discworld/assets/dice/fancy-dice.png" alt="d8" class="dice-icon">
-                ${game.i18n.localize('application.roll')} d8
-              </button>
-              <button type="button" class="roll-button" data-dice="d10">
-                <img src="/systems/discworld/assets/dice/dice.png" alt="d10" class="dice-icon">
-                ${game.i18n.localize('application.roll')} d10
-              </button>
-              <button type="button" class="roll-button" data-dice="d12">
-                <img src="/systems/discworld/assets/dice/dice.png" alt="d12" class="dice-icon">
-                ${game.i18n.localize('application.roll')} d12
-              </button>
-              <button type="button" class="roll-button" data-dice="d20">
-                <img src="/systems/discworld/assets/dice/dice.png" alt="d20" class="dice-icon">
-                ${game.i18n.localize('application.roll')} d20
-              </button>
-              <button type="button" class="roll-button" data-dice="d100">
-                <img src="/systems/discworld/assets/dice/dice.png" alt="d100" class="dice-icon">
-                ${game.i18n.localize('application.roll')} d100
-              </button>
-            </div>
-          </div>
+          <button type="button" class="roll-button" data-dice="d4">
+            <img src="/systems/discworld/assets/dice/dice.png" alt="d4" class="dice-icon">
+            ${game.i18n.localize('application.roll')} d4
+          </button>
+          <button type="button" class="roll-button" data-dice="d6">
+            <img src="/systems/discworld/assets/dice/dice.png" alt="d6" class="dice-icon">
+            ${game.i18n.localize('application.roll')} d6
+          </button>
+          <button type="button" class="roll-button" data-dice="d8">
+            <img src="/systems/discworld/assets/dice/fancy-dice.png" alt="d8" class="dice-icon">
+            ${game.i18n.localize('application.roll')} d8
+          </button>
+          <button type="button" class="roll-button" data-dice="d10">
+            <img src="/systems/discworld/assets/dice/dice.png" alt="d10" class="dice-icon">
+            ${game.i18n.localize('application.roll')} d10
+          </button>
+          <button type="button" class="roll-button" data-dice="d12">
+            <img src="/systems/discworld/assets/dice/dice.png" alt="d12" class="dice-icon">
+            ${game.i18n.localize('application.roll')} d12
+          </button>
+          <button type="button" class="roll-button" data-dice="d20">
+            <img src="/systems/discworld/assets/dice/dice.png" alt="d20" class="dice-icon">
+            ${game.i18n.localize('application.roll')} d20
+          </button>
+          <button type="button" class="roll-button" data-dice="d100">
+            <img src="/systems/discworld/assets/dice/dice.png" alt="d100" class="dice-icon">
+            ${game.i18n.localize('application.roll')} d100
+          </button>
           <br>
         </form>
       `,
@@ -82,7 +63,14 @@ export class DiscRoller {
   }
 }
 
-Hooks.on('renderSceneControls', (controls, html) => {
-  console.log('DiscRoller here', html);
-  DiscRoller.Init(controls, html);
+Hooks.on('getSceneControlButtons', controls => {
+  controls.DiscRoller = {
+    name: "DiscRoller",
+    title: game.i18n.localize('application.discworlddiceroller'),
+    icon: "systems/discworld/assets/dice/fancy-dice.png",
+    onChange: (event, active) => {
+      DiscRoller.CreateDiceRoller(event);
+    },
+    button: true
+  };
 });
